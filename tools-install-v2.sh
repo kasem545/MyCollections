43aca5bb8f201c8f34eca74935e5487065f1be21
Kasem Shibli <kasem545@proton.me>
2025-11-04 23:39:42 +0200

    Update tools-install-v2.sh

--- tools-install-v2.sh
+++ tools-install-v2.sh
@@ -66,7 +66,7 @@ REPOS=(
   "https://github.com/Greenwolf/ntlm_theft.git"
 )
 
-APT_PACKAGES=(curl wget gcc make build-essential golang-go python3 python3-pip python3-venv pipx parallel jq unzip git docker.io docker-compose cargo)
+APT_PACKAGES=(curl wget gcc make build-essential golang-go python3 python3-pip python3-venv pipx parallel jq unzip git docker.io docker-compose cargo ligolo-mp )
 
 # ========== Utilities ==========
 repo_name_from_url() {
