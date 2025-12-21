1487e7278005ac664ebd21d88c00d718febf5336
Kasem Shibli <kasem545@proton.me>
2025-12-21 21:13:57 +0200

    fix

--- hackpad.c
+++ hackpad.c
@@ -76,7 +76,7 @@ void status_msg(const char *msg) {
 
 /* ---------------- SAVE/LOAD ---------------- */
 
-void save_hackpad(HackPad *nb, const char *file) {
+void save_HackPad(HackPad *nb, const char *file) {
     FILE *f = fopen(file, "w");
     if (!f) {
         status_msg("ERROR: Could not save file!");
@@ -100,7 +100,7 @@ void save_hackpad(HackPad *nb, const char *file) {
     status_msg("Saved successfully!");
 }
 
-void load_hackpad(HackPad *nb, const char *file) {
+void load_HackPad(HackPad *nb, const char *file) {
     FILE *f = fopen(file, "r");
     if (!f) return;
 
@@ -209,279 +209,50 @@ void draw_entries(WINDOW *w, HackPad *nb) {
 /* ---------------- LINE EDITOR ---------------- */
 
 int line_editor(const char *title, char *buf, int max_len) {
-    int len = strlen(buf);
-    int estimated_lines = 1;
-    int current_line_len = 0;
-    int max_line_width = COLS - 10;
-    
-    for (int i = 0; i < len; i++) {
-        if (buf[i] == '\n') {
-            estimated_lines++;
-            current_line_len = 0;
-        } else if (buf[i] != '\r') {
-            current_line_len++;
-            if (current_line_len >= max_line_width) {
-                estimated_lines++;
-                current_line_len = 0;
-            }
-        }
-    }
-    
-    estimated_lines += 5;
-    
-    int h = estimated_lines + 4; 
-    if (h < 10) h = 10;
-    if (h > LINES - 4) h = LINES - 4;
-    
-    int w = COLS - 6;
-    int y = (LINES - h) / 2;
-    int x = 3;
+    int h = 7, w = COLS - 6;
+    int y = (LINES - h) / 2, x = 3;
 
     WINDOW *win = newwin(h, w, y, x);
     box(win, 0, 0);
     keypad(win, TRUE);
     curs_set(1);
-    
-    printf("\033[?2004h");
-    fflush(stdout);
 
+    mvwprintw(win, 0, 2, " %s ", title);
+    mvwprintw(win, 4, 2, "Enter:Save  ESC:Cancel");
+
+    int len = strlen(buf);
     int cur = len;
-    int scroll_offset = 0;
     int ch;
 
     while (1) {
-
-        int curr_h = getmaxy(win);
-        int curr_w = getmaxx(win);
-        
         werase(win);
         box(win, 0, 0);
-        
         mvwprintw(win, 0, 2, " %s ", title);
-        
-        int max_display_lines = curr_h - 4;
-        int max_width = curr_w - 4;
-        
-        int total_lines = 1;
-        int cursor_line = 0;
-        int cursor_col = 0;
-        int current_col = 0;
-        
-        for (int i = 0; i <= len; i++) {
-            if (i == cur) {
-                cursor_line = total_lines - 1;
-                cursor_col = current_col;
-            }
-            
-            if (i < len) {
-                if (buf[i] == '\n') {
-                    total_lines++;
-                    current_col = 0;
-                } else if (buf[i] != '\r') {
-                    current_col++;
-                    if (current_col >= max_width) {
-                        total_lines++;
-                        current_col = 0;
-                    }
-                }
-            }
-        }
-        
-        int needed_height = total_lines + 6;
-        if (needed_height > curr_h && needed_height <= LINES - 4) {
-            delwin(win);
-            h = needed_height;
-            if (h > LINES - 4) h = LINES - 4;
-            y = (LINES - h) / 2;
-            win = newwin(h, w, y, x);
-            keypad(win, TRUE);
-            max_display_lines = h - 4;
-            continue; 
-        }
-        
-        if (cursor_line < scroll_offset) {
-            scroll_offset = cursor_line;
-        }
-        if (cursor_line >= scroll_offset + max_display_lines) {
-            scroll_offset = cursor_line - max_display_lines + 1;
-        }
-        
-        int display_y = 2;
-        int current_line = 0;
-        int display_col = 0;
-        int screen_cursor_y = 2;
-        int screen_cursor_x = 2;
-        
-        for (int i = 0; i < len && display_y < curr_h - 2; i++) {
-            if (current_line >= scroll_offset && current_line < scroll_offset + max_display_lines) {
-                if (i == cur) {
-                    screen_cursor_y = display_y;
-                    screen_cursor_x = 2 + display_col;
-                }
-                
-                if (buf[i] == '\n') {
-                    display_y++;
-                    display_col = 0;
-                    current_line++;
-                } else if (buf[i] != '\r') {
-                    if (display_col >= max_width) {
-                        display_y++;
-                        display_col = 0;
-                        current_line++;
-                    }
-                    if (current_line >= scroll_offset && current_line < scroll_offset + max_display_lines && display_y < curr_h - 2) {
-                        mvwaddch(win, display_y, 2 + display_col, buf[i]);
-                    }
-                    display_col++;
-                }
-            } else {
-
-                if (buf[i] == '\n') {
-                    current_line++;
-                    display_col = 0;
-                } else if (buf[i] != '\r') {
-                    display_col++;
-                    if (display_col >= max_width) {
-                        current_line++;
-                        display_col = 0;
-                    }
-                }
-            }
-        }
-        
-        if (cur == len && current_line >= scroll_offset && current_line < scroll_offset + max_display_lines) {
-            screen_cursor_y = display_y;
-            screen_cursor_x = 2 + display_col;
-        }
-        
-        if (has_colors()) wattron(win, COLOR_PAIR(3));
-        if (scroll_offset > 0) {
-            mvwprintw(win, 1, curr_w - 4, "^^^");
-        }
-        if (scroll_offset + max_display_lines < total_lines) {
-            mvwprintw(win, curr_h - 3, curr_w - 4, "vvv");
-        }
-        if (has_colors()) wattroff(win, COLOR_PAIR(3));
-        
-        if (has_colors()) wattron(win, COLOR_PAIR(2));
-        mvwprintw(win, curr_h - 2, 2, "Alt+S:Save ESC:Cancel | Ln %d/%d Ch %d/%d",
-                  cursor_line + 1, total_lines, len, max_len - 1);
-        if (has_colors()) wattroff(win, COLOR_PAIR(2));
-        
-        wmove(win, screen_cursor_y, screen_cursor_x);
+        mvwprintw(win, 2, 2, "%s", buf);
+        mvwprintw(win, 4, 2, "Enter:Save  ESC:Cancel");
+        wmove(win, 2, 2 + cur);
         wrefresh(win);
 
         ch = wgetch(win);
 
         if (ch == 27) { 
-            nodelay(win, TRUE);
-            int next_ch = wgetch(win);
-            
-            if (next_ch == '[') {
-
-                int third_ch = wgetch(win);
-                if (third_ch == '2') {
-                    int fourth_ch = wgetch(win);
-                    if (fourth_ch == '0') {
-                        int fifth_ch = wgetch(win);
-                        if (fifth_ch == '0') {
-                            int sixth_ch = wgetch(win);
-                            if (sixth_ch == '~') {
-                                nodelay(win, FALSE);
-                                continue;
-                            }
-                        } else if (fifth_ch == '1') {
-                            int sixth_ch = wgetch(win);
-                            if (sixth_ch == '~') {
-                                nodelay(win, FALSE);
-                                continue;
-                            }
-                        }
-                    }
-                }
-            } else if (next_ch == 's' || next_ch == 'S') {
-                nodelay(win, FALSE);
-                printf("\033[?2004l");
-                fflush(stdout);
-                delwin(win);
-                curs_set(0);
-                return buf[0] != '\0';
-            } else if (next_ch == ERR) {
-                nodelay(win, FALSE);
-                printf("\033[?2004l");
-                fflush(stdout);
-                delwin(win);
-                curs_set(0);
-                return 0;
-            }
-            
-            nodelay(win, FALSE);
-            continue;
+            delwin(win);
+            curs_set(0);
+            return 0;
         }
-        if (ch == '\n' || ch == KEY_ENTER || ch == 10) {
-            if (len < max_len - 1) {
-                memmove(&buf[cur + 1], &buf[cur], len - cur + 1);
-                buf[cur++] = '\n';
-                len++;
-            }
+        if (ch == '\n') {
+            delwin(win);
+            curs_set(0);
+            return buf[0] != '\0';
         }
-        else if (ch == '\r') {
-            if (len < max_len - 1) {
-                memmove(&buf[cur + 1], &buf[cur], len - cur + 1);
-                buf[cur++] = '\n';
-                len++;
-            }
-        }
-        else if (ch == KEY_LEFT && cur > 0) 
+        if (ch == KEY_LEFT && cur > 0) 
             cur--;
         else if (ch == KEY_RIGHT && cur < len) 
             cur++;
-        else if (ch == KEY_UP) {
-            int line_start = cur;
-            while (line_start > 0 && buf[line_start - 1] != '\n') line_start--;
-            if (line_start > 0) {
-                int prev_line_start = line_start - 1;
-                while (prev_line_start > 0 && buf[prev_line_start - 1] != '\n') prev_line_start--;
-                int col = cur - line_start;
-                int prev_line_len = line_start - 1 - prev_line_start;
-                cur = prev_line_start + (col < prev_line_len ? col : prev_line_len);
-            }
-        }
-        else if (ch == KEY_DOWN) {
-            int line_start = cur;
-            while (line_start > 0 && buf[line_start - 1] != '\n') line_start--;
-            int next_line_start = cur;
-            while (next_line_start < len && buf[next_line_start] != '\n') next_line_start++;
-            if (next_line_start < len) {
-                next_line_start++;
-                int col = cur - line_start;
-                int next_line_end = next_line_start;
-                while (next_line_end < len && buf[next_line_end] != '\n') next_line_end++;
-                int next_line_len = next_line_end - next_line_start;
-                cur = next_line_start + (col < next_line_len ? col : next_line_len);
-            }
-        }
-        else if (ch == KEY_PPAGE) { 
-            scroll_offset = (scroll_offset > max_display_lines) ? 
-                scroll_offset - max_display_lines : 0;
-            for (int i = 0; i < max_display_lines && cur > 0; i++) {
-                while (cur > 0 && buf[cur - 1] != '\n') cur--;
-                if (cur > 0) cur--;
-            }
-        }
-        else if (ch == KEY_NPAGE) { 
-            scroll_offset += max_display_lines;
-            for (int i = 0; i < max_display_lines && cur < len; i++) {
-                while (cur < len && buf[cur] != '\n') cur++;
-                if (cur < len) cur++;
-            }
-        }
-        else if (ch == KEY_HOME) {
-            while (cur > 0 && buf[cur - 1] != '\n') cur--;
-        }
-        else if (ch == KEY_END) {
-            while (cur < len && buf[cur] != '\n') cur++;
-        }
+        else if (ch == KEY_HOME)
+            cur = 0;
+        else if (ch == KEY_END)
+            cur = len;
         else if ((ch == KEY_BACKSPACE || ch == 127 || ch == 8) && cur > 0) {
             memmove(&buf[cur - 1], &buf[cur], len - cur + 1);
             cur--; len--;
@@ -490,9 +261,9 @@ int line_editor(const char *title, char *buf, int max_len) {
             memmove(&buf[cur], &buf[cur + 1], len - cur);
             len--;
         }
-        else if ((isprint(ch) || ch == '\t') && len < max_len - 1) {
+        else if (isprint(ch) && len < max_len - 1) {
             memmove(&buf[cur + 1], &buf[cur], len - cur + 1);
-            buf[cur++] = (ch == '\t') ? ' ' : ch;
+            buf[cur++] = ch;
             len++;
         }
     }
@@ -654,7 +425,7 @@ void copy_to_clipboard(HackPad *nb) {
 }
 
 void quick_save(HackPad *nb) {
-    save_hackpad(nb, nb->filename);
+    save_HackPad(nb, nb->filename);
 }
 
 void save_as(HackPad *nb) {
@@ -663,7 +434,7 @@ void save_as(HackPad *nb) {
     
     if (line_editor("Save As", newfile, 256)) {
         strncpy(nb->filename, newfile, 255);
-        save_hackpad(nb, nb->filename);
+        save_HackPad(nb, nb->filename);
     }
 }
 
@@ -675,7 +446,7 @@ int main(int argc, char *argv[]) {
     
     const char *file = (argc > 1) ? argv[1] : "HackPad.md";
 
-    load_hackpad(&nb, file);
+    load_HackPad(&nb, file);
     strncpy(nb.filename, file, 255);
 
     if (nb.section_count == 0) {
@@ -732,14 +503,14 @@ int main(int argc, char *argv[]) {
                     s->selected_entry++;
                 break;
 
-            case KEY_PPAGE: 
+            case KEY_PPAGE:
                 if (nb.focus == FOCUS_ENTRIES && s->entry_count > 0) {
                     s->selected_entry = (s->selected_entry > 10) ?
                         s->selected_entry - 10 : 0;
                 }
                 break;
 
-            case KEY_NPAGE: 
+            case KEY_NPAGE:
                 if (nb.focus == FOCUS_ENTRIES && s->entry_count > 0) {
                     s->selected_entry = (s->selected_entry + 10 < s->entry_count) ?
                         s->selected_entry + 10 : s->entry_count - 1;
@@ -790,7 +561,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (confirm_dialog("Save before quitting?")) {
-        save_hackpad(&nb, nb.filename);
+        save_HackPad(&nb, nb.filename);
     }
 
     ui_shutdown();
