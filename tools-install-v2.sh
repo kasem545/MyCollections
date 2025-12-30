e1c0f4cfd672443db663c4b190ec1a138bfd4a52
Kasem Shibli <kasem545@proton.me>
2025-12-30 14:21:08 +0200

    Update tools-install-v2.sh

--- tools-install-v2.sh
+++ tools-install-v2.sh
@@ -66,6 +66,7 @@ REPOS=(
   "https://github.com/Adaptix-Framework/AdaptixC2.git"
   "https://github.com/itm4n/PrivescCheck.git"
   "https://github.com/diego-treitos/linux-smart-enumeration.git"
+  "https://github.com/ShutdownRepo/targetedKerberoast.git"
 )
 
 APT_PACKAGES=(curl wget gcc make build-essential fzf golang-go python3 python3-pip python3-venv pipx parallel jq unzip git docker.io docker-compose cargo zsh tmux ligolo-mp)
