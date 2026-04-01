e0700b46caaa592ce79b86b611dde67d2fe1e6a9
Kasem Shibli <kasem545@proton.me>
2026-04-01 17:14:20 +0300

    Update tools-install-kali.sh

--- scripts/tools-install-kali.sh
+++ scripts/tools-install-kali.sh
@@ -74,7 +74,7 @@ REPOS=(
   "https://github.com/strayge/pylnk.git"
 )
 
-APT_PACKAGES=(curl wget gcc make build-essential fzf golang-go python3 python3-pip python3-venv pipx parallel jq unzip git docker.io docker-compose cargo zsh tmux ligolo-mp lsd)
+APT_PACKAGES=(curl wget gcc make build-essential fzf fish fastfetch lsd golang-go python3 python3-pip python3-venv pipx parallel jq unzip git docker.io docker-compose cargo zsh tmux ligolo-mp)
 
 # ========== Utilities ==========
 repo_name_from_url() {
