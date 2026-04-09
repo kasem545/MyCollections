03f3a7bc9b28f42ea9865a9e22ec59b5677ef94b
Kasem Shibli <kasem545@proton.me>
2026-04-09 11:16:27 +0300

    Update tools-install-kali.sh

--- scripts/tools-install-kali.sh
+++ scripts/tools-install-kali.sh
@@ -73,6 +73,7 @@ REPOS=(
   "https://github.com/semgrep/semgrep-rules.git"
   "https://github.com/nullsection/chisel-ng.git"
   "https://github.com/strayge/pylnk.git"
+  "https://github.com/nikaiw/VMkatz.git"
 )
 
 APT_PACKAGES=(curl wget gcc make build-essential fzf fish fastfetch lsd golang-go python3 python3-pip python3-venv pipx parallel jq unzip git docker.io docker-compose cargo zsh tmux ligolo-mp)
